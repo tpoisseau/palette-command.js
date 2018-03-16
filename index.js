78b4202b01a2f03e671e90631cee651e1a376805
tpoisseau <tpoisseau@vvfvillages.fr>
2018-03-16 11:38:59 +0100

    Packaging du CSS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import CommandPalette from './src/CommandPalette';
+import './src/CommandPalette.scss';
 
 export default CommandPalette;
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import sass from 'rollup-plugin-sass';
 
 export default [
   {
     input: 'index.js',
     plugins: [
+      sass({
+        options: {
+          outputStyle: 'compressed',
+          sourceMapContents: true,
+          sourceMapEmbed: true,
+          sourceMap: true,
+          outFile: 'dist/command-palette.min.css',
+        },
+        output: 'dist/command-palette.min.css',
+      }),
       babel({
         exclude: 'node_modules/**',
       }),
       uglify({
         compress: {
-          drop_console: true,
+          drop_console: false,
         },
       }),
     ],
